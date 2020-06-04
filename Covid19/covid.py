d4ee2cc4de89ad78b4d72d88982dfc8c9b34e33a
qackerma <quentin.ackermann@c-s.fr>
2020-06-04 14:24:37 +0200

    Update

--- Covid19/covid.py
+++ Covid19/covid.py
@@ -43,10 +43,11 @@ def checkNan(df):
     return df.isna()
 
 
-def constructHeatMap(data):
+def constructHeatMap(data, show=False):
     plt.figure(figsize=(20, 10))
     sns.heatmap(data, cbar=False)
-    plt.show()
+    if show:
+        plt.show()
 
 
 def missing_values_percentage(df):
@@ -107,6 +108,42 @@ def display_relations(column_name, relation):
     plt.show()
 
 
+def count_histogram(df, x, hue):
+    sns.countplot(x=x, hue=hue, data=df)
+    plt.show()
+
+
+def crossTable(df, cross1, cross2):
+    return pd.crosstab(df[cross1], df[cross2])
+
+
+def crossTables(df, column_name, cross):
+    for col in column_name:
+        plt.figure()
+        sns.heatmap(pd.crosstab(df[cross], df[col]), annot=True, fmt='d')
+    plt.show()
+
+
+def pairwise_relationships(df, variable, cluster=True):
+    # sns.pairplot(df[variable])
+    if cluster:
+        sns.clustermap(df[variable].corr())
+    else:
+        sns.heatmap(df[variable].corr())
+    plt.show()
+
+
+def view_regression(df, column_name, absc, discrimination):
+    for col in column_name:
+        plt.figure()
+        sns.lmplot(x=absc, y=col, hue=discrimination, data=df)
+    plt.show()
+
+
+def check_correlation(df, value_for_correlation):
+    return df.corr()[value_for_correlation].sort_values()
+
+
 if __name__ == "__main__":
     df = load_dataset(dataset_path=DATASET_PATH)
     # general_info(df)
@@ -119,22 +156,38 @@ if __name__ == "__main__":
     # analyse_target(df, "SARS-Cov-2 exam result", True)
     # draw_histograms(df, 'object')
 
-
     """
     Target/Variables relation :
     """
-    ### Positive and Negative collections
+    # Positive and Negative collections
     positive_df = qual_to_quan(df, "SARS-Cov-2 exam result", 'positive')
     negative_df = qual_to_quan(df, "SARS-Cov-2 exam result", 'negative')
 
-    ### Blood and Viral collections
+    # Blood and Viral collections
     MR = missing_rate(df)
     blood_columns = rate_borned(df, MR, 0.88, 0.9)
     viral_columns = rate_borned(df, MR, 0.75, 0.88)
 
     relation = [(positive_df, 'positive'), (negative_df, 'negative')]
-    display_relations(blood_columns, relation)
+    # display_relations(blood_columns, relation)
 
+    # Relation Target and Age
+    # count_histogram(df, 'Patient age quantile', 'SARS-Cov-2 exam result')
 
+    # Relation, comparison between collection : Target and Viral
+    # print(crossTable(df, 'SARS-Cov-2 exam result', 'Influenza A'))
+    # crossTables(df, viral_columns, "SARS-Cov-2 exam result")
 
+    """
+    Advanced anlysis
+    """
 
+    # Blood / Blood relations
+    # pairwise_relationships(df, blood_columns)
+
+    # Blood / Age relations view_regression(df, blood_columns, "Patient age quantile", "SARS-Cov-2 exam result")  # A
+    # bit messy, using correslation instead, could be used for more analysis print(check_correlation(df, "Patient age
+    # quantile"))  # Check if age is correlated with anything ?
+
+    # Blood / Age relations
+    crossTable(df, 'Influenza A', 'Influenza A, rapid test')
